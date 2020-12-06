c5cd2c718cc37ad05dc98dc77bca835e73072884
David Achee <david.achee@gmail.com>
2020-12-06 14:23:41 -0600

    Add prettier, update wording and styles

--- components/inline-link.tsx
+++ components/inline-link.tsx
@@ -1,8 +1,23 @@
 interface Props {
-    href: string
-    text: string
+  href: string
+  text: string
+  className?: string
 }
 
 export default function InlineLink(props: Props) {
-    return (<a className="hover:text-yellow-300 dark:hover:text-red-400 transition-colors duration-300 underline" href={props.href}>{props.text}</a>)
+  return (
+    <a
+      className={`
+        hover:text-yellow-300
+        dark:hover:text-red-400
+        transition-colors
+        duration-300
+        underline
+        ${props.className}
+      `}
+      href={props.href}
+    >
+      {props.text}
+    </a>
+  )
 }
