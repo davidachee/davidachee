0f32d39fb9b7ffd108489f257a77515dff614edd
David Achee <959180+davidachee@users.noreply.github.com>
2024-07-23 10:57:19 -0400

    Update copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ export default function IndexPage() {
         <p className="font-mono font-light pt-4 text-xl max-w-2xl text-white">
           <InlineLink className="pr-4" href="mailto:hi@davidachee.com" text="email" />
           <InlineLink className="pr-4" href="https://github.com/davidachee" text="github" />
-          <InlineLink className="pr-4" href="https://twitter.com/davidachee" text="twitter" />
           <InlineLink
             className="pr-4"
             href="https://www.linkedin.com/in/david-achee-14380338/"
@@ -17,9 +16,10 @@ export default function IndexPage() {
         </p>
         <p className="font-mono font-light pt-4 text-xl max-w-2xl text-white">👋 Hi there!</p>
         <p className="font-mono font-light pt-4 text-xl max-w-2xl text-white">
-          I'm a full-stack engineer with over ten years experience building production ready
-          applications from the ground up. Most recently, my focus has been on designing and
-          implementing multi-player collaborative experiences. I have particular expertise with{' '}
+          I'm a full-stack engineer with over thirteen years of experience building production ready
+          applications from the ground up. Most recently, I have been working as the technical lead
+          of the Compensation team at <InlineLink href="https://www.lattice.com/" text="Lattice" />.
+          I have particular expertise with{' '}
           <strong className="font-bold">
             Typescript, React, GraphQL, Jest, Express, nodejs and Postgres
           </strong>
@@ -28,12 +28,9 @@ export default function IndexPage() {
         <p className="font-mono font-light pt-4 text-xl max-w-2xl text-white">
           My strong design background allows me to identify problems before the development process
           begins. I pride myself on my ability to work with product and design to find the best
-          possible version of what we're building. I'm happiest collaborating with team members to
-          create something amazing together.
-        </p>
-        <p className="font-mono font-light pt-4 text-xl max-w-2xl text-white">
-          I'm currently a software engineer at{' '}
-          <InlineLink href="https://www.lattice.com/" text="Lattice" />.
+          possible version of what we're building. I love taking large, abstract problems and
+          breaking them down into actionable milestones. I'm happiest collaborating with team
+          members to create something amazing together.
         </p>
       </div>
     </>
